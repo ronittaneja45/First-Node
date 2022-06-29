9f63d92e1a753ed9f04121598d219c539a61c727
ronittaneja45 <ronittaneja45@gmail.com>
2022-06-29 18:16:50 +0530

    3 files modified

--- index.js
+++ index.js
@@ -3,10 +3,14 @@
 /**
  * Required External Modules
  */
+const express = require('express');
+const path = require('path');
 
 /**
  * App Variables
  */
+const app = express();
+const port = process.env.PORT || '8000';
 
 /**
  *  App Configuration
@@ -15,7 +19,13 @@
 /**
  * Routes Definitions
  */
+app.get('/', (req, res) => {
+  res.status(200).send('WHATABYTE: Food For Devs');
+});
 
 /**
  * Server Activation
  */
+app.listen(port, () => {
+  console.log(`Listening to requests on http://localhost:${port}`);
+});
